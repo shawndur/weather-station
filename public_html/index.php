aff4c3444a445f7c77fa9763984c2be7efafc1eb
shawn <shawndur@buffalo.edu>
2017-08-29 00:43:36 -0400

    connect to db

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,15 @@
 <?php
-  
+  $address = 'localhost';
+  $username = 'weather';
+  $password = 'station';
+  $dbname = 'weather-station';
+  $conn = mysql_connect($address, $username, $password);
+  if (!$conn) {
+    die("Could not connect to MySql")
+  }
+  if (!mysql_select_db($dbname)) {
+    die("Database could not be selected");
+  }
+
+  mysql_close($conn);
 ?>
