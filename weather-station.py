d8245bd57fcb94f41d1955d77dcc126ba79bcfe2
shawn <shawndur@buffalo.edu>
2017-08-28 21:03:32 -0400

    initial program layout

--- weather-station.py
+++ weather-station.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
 from sense_hat import SenseHat
+import PyMySql
+
+#initialize program
+ #load config data
+ #connect to mysql server
+ #check if db exists
+    #if not create db
+
+#main loop
+ #every time unit
+  #get data
+   #get temp from pressure
+   #get temp from humidity
+   #find average temp
+   #get pressure
+   #get humidity
+  #store data in server
