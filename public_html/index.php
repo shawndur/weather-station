88374f6fb41281d4a14e3e5f30fbd9438af565ef
shawn <shawndur@buffalo.edu>
2017-08-29 00:58:52 -0400

    added reading of data from db

--- public_html/index.php
+++ public_html/index.php
@@ -3,12 +3,20 @@
   $username = 'weather';
   $password = 'station';
   $dbname = 'weather-station';
+
   $conn = mysql_connect($address, $username, $password);
   if (!$conn) {
-    die("Could not connect to MySql")
+    die("Could not connect to MySql" . mysql_error())
   }
+
   if (!mysql_select_db($dbname)) {
-    die("Database could not be selected");
+    die("Database could not be selected: " . mysql_error());
+  }
+
+  $sql = 'SELECT * FROM READINGS WHERE reading_time > now() - INTERVAL 1 DAY';
+  $res = mysql_query($sql, $conn);
+  if (!res) {
+    die("Could not load data" . mysql_error());
   }
 
   mysql_close($conn);
